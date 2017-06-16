db405c8e058759a5cd4aa160d29c164ea4a5d762
mc-tangb <511598230@qq.com>
2017-06-16 16:57:34 +0800

    cm

---
+++ src/js/core/evenful.js
@@ -0,0 +1,214 @@
+let arraySlice = Array.prototype.slice;
+
+let Eventful = function () {
+	this._$handlers = {};
+}
+
+Eventful.prototype = {
+	constructor: Eventful,
+	/**
+	 * 单次绑定事件, trigger 后销毁
+	 * 
+	 * @param {string} event 事件名
+	 * @param {function} handler 响应函数
+	 * @param {Object} context
+	 */
+	one: function(event, handler, context) {
+		let _h = this._$handlers;
+
+		if(!handler || !event) {
+			return this;
+		}
+
+		if(!_h[event]) {
+			_h[event] = [];
+		}
+
+		for(let i=0; i<_h[event].length; i++) {
+			if(_h[event][i].h === handler) {
+				return this;
+			}
+		}
+
+		_h[event].push({
+			h: handler,
+			one: true,
+			ctx: context || this
+		});
+
+		return this;
+	},
+	
+	/**
+	 * 单次绑定事件
+	 * 
+	 * @param {string} event 事件名
+	 * @param {function} handler 响应函数
+	 * @param {Object} context
+	 */
+	on: function(event, handler, context) {
+		let _h = this._$handlers;
+
+		if(!handler || !event) {
+			return this;
+		}
+
+		if(!_h[event]) {
+			_h[event] = [];
+		}
+
+		for(let i=0; i<_h[event].length; i++) {
+			if(_h[event][i].h === handler) {
+				return this;
+			}
+		}
+
+		_h[event].push({
+			h: handler,
+			one: true,
+			ctx: context || this
+		});
+
+		return this;
+	},
+	
+	/**
+	 * 是否绑定了事件
+	 * 
+	 * @param {string} event 事件名
+	 * @return {boolean}
+	 */
+
+	isSilent: function (event) {
+		let _h = this._$handlers;
+		return _h[event] && _h[event].length;
+	},
+	/**
+	 * 解绑事件
+	 * 
+	 * @param {string} event 
+	 * @param {function} handler
+	 */
+
+	off: function(event, handler) {
+		let _h = this._$handlers;
+
+		if (!event) {
+			this._$handlers = {};
+			return this;
+		}
+
+		if (handler) {
+			if(_h[event]) {
+				let newList = [];
+				for(var i = 0, l = _h[event].length; i < l; i++) {
+					if (_h[event][i]['h'] != handler) {
+						newList.push(_h[event][i]);
+					}
+				}
+				_h[event] = newList;
+			}
+
+			if (_h[event] && _h[event].length === 0) {
+				delete _h[event];
+			}
+		} else {
+			delete _h[event];
+		}
+
+		return this;
+	},
+	/**
+	 * 分发事件
+	 * 
+	 * @param {string} type 事件类型
+	 */
+	trigger: function(type) {
+		if(this._$handlers[type]) {
+			let args = arguments;
+			let argLen = args.length;
+
+			if (argLen > 3) {
+				args = arraySlice.call(args, 1);
+			}
+
+			let _h = this._$handlers[type];
+
+			let len = _h.length;
+
+			for (let i=0; i < len; ) {
+				switch (argLen) {
+					case 1:
+						_h[i]['h'].call(_h[i]['ctx']);
+						break;
+					case 2:
+						_h[i]['h'].call(_h[i]['ctx'], args[1]);
+						break;
+					case 3:
+						_h[i]['h'].call(_h[i]['ctx'], args[1], args[2]);
+						break;
+					default:
+						_h[i]['h'].apply(_h[i]['ctx'], args);
+						break;
+				}
+
+				if(_h[i]['one']) {
+					_h.splice(i, 1);
+					len --;
+				} else {
+					i++;
+				}
+			}
+		}
+
+		return this;
+	}
+	/**
+	 * 分发事件, 有context的事件
+	 * 
+	 * @param {string} type 事件类型
+	 */
+	triggerWithContext: function (type) {
+		if(this._$handlers[type]) {
+			let args = arguments;
+			let argLen = args.length;
+
+			if (argLen > 4) {
+				args = arraySlice.call(args, 1, args.length - 1);
+			}
+			let ctx = args[args.length - 1];
+
+			let _h = this._$handlers[type];
+
+			let len = _h.length;
+
+			for (let i=0; i < len; ) {
+				switch (argLen) {
+					case 1:
+						_h[i]['h'].call(ctx);
+						break;
+					case 2:
+						_h[i]['h'].call(ctx, args[1]);
+						break;
+					case 3:
+						_h[i]['h'].call(ctx, args[1], args[2]);
+						break;
+					default:
+						_h[i]['h'].apply(ctx, args);
+						break;
+				}
+
+				if(_h[i]['one']) {
+					_h.splice(i, 1);
+					len --;
+				} else {
+					i++;
+				}
+			}
+		}
+
+		return this;
+	}
+}
+
+export {Eventful}
---
+++ src/js/core/event.js
@@ -0,0 +1,94 @@
+import { env } from './env'
+import { Eventful } from './eventful'
+
+let isDomLevel2 = (typeof window !== 'undefined') && !!window.addEventListener;
+
+function getBoundingClientRect(el) {
+	return el.getBoundingClientRect ? el.getBoundingClientRect() : {left: 0, top: 0};	
+}
+
+function clientToLocal(el, e, out, calculate) {
+	out = out || {};
+
+	if(calculate || !env.canvasSupported) {
+		defaultGetZrXY(el, e, out);
+	// firefox
+	} else if (env.browser.firefox && e.layerX != null && e.layerX !== e.offsetX) {
+		out.zrX = e.layerX;
+		out.zrY = e.layerY;
+	// IE6+, chrome, safari, opera
+	} else if (e.offsetX != null) {
+		out.zrX = e.offsetX;
+		out.zrY = e.offsetY;
+	// IOS safari
+	} else {
+		defaultGetZrXY(el, e, out);
+	}
+
+	return out;
+}
+
+function defaultGetZrXY(el, e, out) {
+	let box = getBoundingClientRect(el);
+	out.zrX = e.clientX - box.left;
+	out.zrY = e.clientY - box.top;
+}
+
+function normalizeEvent(el, e, calculate) {
+	e = e || window.event;
+
+	if (e.zrX != null) {
+		return e;
+	}
+
+	let eventType = e.type;
+	let isTouch = eventType && eventType.indexOf('touch') >= 0;
+
+	if (!isTouch) {
+		clientToLocal(el, e, e, calculate);
+		e.zrDelta = (e.wheelDelta) ? e.wheelDelta / 120 : -(e.detail || 0) / 3;
+	} else {
+		var touch = eventType != 'touchend'
+			? e.targetTouches[0]
+			: e.changedTouches[0];
+		touch && clientToLocal(el, touch, e, calculate);
+	}
+
+	return e;
+}
+
+function addEventListener(el, name, handler) {
+	if (isDomLevel2) {
+		el.addEventListener(name, handler);
+	} else {
+		el.attachEvent('on' + name, handler);
+	}
+}
+
+function removeEventListener(el, name, handler) {
+	if (isDomLevel2) {
+		el.removeEventListener(name, handler);
+	} else {
+		el.detachEvent('on' + name, handler);
+	}
+}
+
+let stop = isDomLevel2
+	? function (e) {
+		e.preventDefault();
+		e.stopPropagation();
+		e.cancleBubble = true;
+	}
+	: function (e) {
+		e.returnValue = false;
+		e.cancleBubble = true;
+	};
+
+export {
+	clientToLocal: clientToLocal,
+	normalizeEvent: normalizeEvent,
+	addEventListener: addEventListener,
+	removeEventListener: removeEventListener,
+	stop: stop,
+	Dispatcher: Eventful
+}
