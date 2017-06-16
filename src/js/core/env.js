e1317bc54653092a1b07af6beb5a38d723b63e02
mc-tangb <511598230@qq.com>
2017-06-16 09:52:33 +0800

    浏览器环境判断

---
+++ src/js/core/env.js
@@ -0,0 +1,61 @@
+let env = {};
+
+if(typeof navigator === 'undefined') {
+	env = {
+		browser: {},
+		os: {},
+		node: true,
+		canvasSupported: true
+	}
+} else {
+	env = detect(navigator.userAgent);
+}
+
+function detect(ua) {
+	let os = {};
+
+	let browser = {};
+
+	let firefox = ua.match(/Firefox\/([\d.]+)/);
+
+	let ie = ua.match(/MSIE\s([\d.]+)/) || ua.match(/Trident\/.+?rv:([\d.]+)/);
+
+	let edge = ua.match(/Edge\/([\d.]+)/);
+
+	let weChat = (/micromessenger/i).test(ua);
+
+	if (firefox) {
+		browser.firefox = true;
+		browser.version = firefox[1];
+	}
+
+	if (ie) {
+		browser.ie = true;
+		browser.version = ie[1];
+	}
+
+	if (edge) {
+		browser.edge = true;
+		browser.version = edge[1];
+	}
+
+	if (weChat) {
+		browser.weChat = true;
+	}
+
+	return {
+		browser: browser,
+
+		os: os,
+
+		node: false,
+
+		canvasSupported: document.createElement('canvas').getContext ? true : false,
+
+		touchEventsSupported: 'ontouchstart' in window && !browser.ie && !browser.edge,
+
+		pointerEventsSupported: 'onpointerdown' in $window && (browser.edge || (browser.ie && browser.version >= 11))
+	}
+}
+
+export {env}
